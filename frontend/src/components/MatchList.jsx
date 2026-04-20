49a81fe749fac25e3718476b2e3adfb2c9ab724e
Naman <namansahu311202@gmail.com>
2026-04-20 22:36:44 +0530

    fix case issue

--- frontend/src/components/MatchList.jsx
+++ frontend/src/components/MatchList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import LogoutButton from "./LogoutButton";
+import LogoutButton  from "./logoutbutton ";
 
 export default function MatchList() {
   const [tab, setTab] = useState("live");
